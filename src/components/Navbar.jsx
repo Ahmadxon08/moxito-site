56ce2aaa535cc1368467540c13ba372e3df7bc5a
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-12-25 13:41:36 +0500

    Implemented Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       { backgroundColor: "tranparent" },
       {
         backgroundColor: "00000050",
-        backgroundFilter: "blur(10ox)",
+        backgroundFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
       }
